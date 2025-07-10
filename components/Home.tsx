ec728deb4c6a590b59b39e72bc6e655fefbe1d67
kakaparubak <kaka.parubak@binus.ac.id>
2025-07-10 17:41:28 +0800

    added "Lacquer" font and changed h1 to "Lacquer"

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-background text-foreground">
-      <h1 className="text-4xl font-bold tracking-tight">Welcome to My Portfolio</h1>
+      <h1 className="text-4xl font-bold tracking-tight font-lacquer">Welcome to My Portfolio</h1>
     </div>
   )
 }
