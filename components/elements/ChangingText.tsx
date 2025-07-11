8ab62888c454990582fa3183ae37c0afe21595f2
jackdafox <83963164+jackdafox@users.noreply.github.com>
2025-07-11 11:18:23 +0800

    added data.ts

--- components/elements/ChangingText.tsx
+++ components/elements/ChangingText.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
+import { roles } from "../../data";
 
 const ChangingText = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % roles.length);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <p className="text-center font-montserrat text-3xl font-medium mt-7 tracking-tighter text-white">
       and I am a{" "}
       <span className="underline font-covgrace tracking-wider text-cyan-400 text-4xl">
-        Lighting Engineer.
+        {roles[index]}
       </span>
     </p>
   );
---
+++ data.ts
@@ -0,0 +1,5 @@
+export const roles = [
+  "Web Developer",
+  "Lighting Engineer",
+  "dadang"
+];
